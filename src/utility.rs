c51ad03f8e338320bc33024aa5541f5ac30d2656
boots <sadasf@axfas.asfas>
2026-01-17 19:23:27 +0100

    modified random_range

--- src/utility.rs
+++ src/utility.rs
@@ -1,6 +1,5 @@
 use firefly_rust::get_random;
 
-/// Returns a random u32 in the range [min, max] (inclusive).
 pub fn random_range(min: u32, max: u32) -> u32 {
     if min >= max {
         return min;
